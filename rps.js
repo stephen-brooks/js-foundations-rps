822f5e34500c11716b878072c68669e625948365
Stephen Brooks <75581182+stephen-brooks@users.noreply.github.com>
2023-09-14 16:24:10 +0100

    Added front-end and click capabilities to RPS game

--- rps.js
+++ rps.js
@@ -39,45 +39,65 @@ function playRound(playerSelection, computerSelection) {
 
     }
 
+function setScores(){
+    document.getElementById("playerScore").innerHTML = playerScore.getAttribute('data-value');
+    document.getElementById("computerScore").innerHTML = computerScore.getAttribute('data-value');
+};
+
+function playRoundButton() {
+    buttons.forEach((button)  => {
+        
+        button.addEventListener('click', () => {
+            const playerSelection = button.getAttribute('data-value');
+            
+            let computerSelection = getComputerChoice();
+            
+            let res = playRound(playerSelection, computerSelection);
+            
+            if(res == 1){
+                alert(`You WIN ${playerSelection} beats ${computerSelection}`);
+                playerScore.setAttribute('data-value', (+playerScore.getAttribute('data-value') + 1));
+            } 
+            
+            else if(res == 0){
+                alert(`TIE Game, you both chose ${playerSelection}`)
+            } 
+            
+            else{
+                alert(`You LOSE ${computerSelection} beats ${playerSelection}`);
+                computerScore.setAttribute('data-value', (+computerScore.getAttribute('data-value') + 1));
+            }
+            
+            setScores();
+            
+            if (+playerScore.getAttribute('data-value') == 5){
+                alert("Congradulations, you win! Lucky!");
+                document.location.reload();
+                clearInterval(interval);
+                
+            }
+            else if (+computerScore.getAttribute('data-value') == 5){
+                alert ("Uh-oh, you lose... Loser!");
+                document.location.reload();
+                clearInterval(interval);
+            }
+        })
+        });
+    };
+
+
 function game(){
 
-    let score = 0;
-    for(let i = 0; i < 5; i++){ 
-        
-        //let playerSelection = prompt("Make your choice");
-        let playerSelection = getComputerChoice();
-        let computerSelection = getComputerChoice();
-
-        let res = playRound(playerSelection, computerSelection);
-        
-        if(res == 1){
-            console.log(`You win ${playerSelection} beats ${computerSelection}`)
-        } 
-        
-        else if(res == 0){
-            console.log(`Tie Game, you both chose ${playerSelection}`)
-        } 
-        
-        else{
-            console.log(`You lose ${computerSelection} beats ${playerSelection}`)
-        }
-
-        score += res;
-    }
-
-    switch(Math.sign(score)){
-        case 1:
-            console.log("Congradulations, you win");
-            break;
-        case 0:
-            console.log("Tie Game");
-            break;
-        default:
-            console.log("You lose, loser!")
-        
-    }
+    setScores();
+    playRoundButton();
+    setScores();
 
 }
+
+const buttons = document.querySelectorAll('#rps-buttons button');
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
+
 
 game();
 
